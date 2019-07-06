d01c1f14f029d1715ea0ff7df9d73084b413db66
Graham Reeves <graham@grahamreeves.com>
2019-07-06 14:34:00 +0100

    Moved unit test common into PopEngineCommon

--- PopTexture.js
+++ PopTexture.js
@@ -14,3 +14,11 @@ Pop.CreateRandomImage = function(Width,Height)
 	Texture.WritePixels( Width, Height, Pixels, Format );
 	return Texture;
 }
+
+
+Pop.CreateColourTexture = function(Colour4)
+{
+	let NewTexture = new Pop.Image();
+	NewTexture.WritePixels( 1, 1, Colour4 );
+	return NewTexture;
+}
