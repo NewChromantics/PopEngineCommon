e113276dc179b8b2e09e527b74ee76688d2314a7
Graham Reeves <graham@grahamreeves.com>
2019-07-08 19:09:35 +0100

    Added Math.Multiply3

--- PopMath.js
+++ PopMath.js
@@ -134,6 +134,11 @@ Math.Add3 = function(a,b)
 	return [ a[0]+b[0], a[1]+b[1], a[2]+b[2] ];
 }
 
+Math.Multiply3 = function(a,b)
+{
+	return [ a[0]*b[0], a[1]*b[1], a[2]*b[2] ];
+}
+
 Math.Cross3 = function(a,b)
 {
 	let x = a[2] * b[3] - b[2] * a[3];
