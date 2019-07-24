d84d384b0fd5903b8ce0deb83b22234e19f93b08
Graham Reeves <graham@grahamreeves.com>
2019-07-24 14:20:46 +0100

    Added camera orbit controls, +GetPitchYawRollDistance +SetOrbit

--- PopCamera.js
+++ PopCamera.js
@@ -105,7 +105,68 @@ Pop.Camera = function()
 		return Matrix;
 	}
 	
-	this.OnCameraPan = function(x,y,FirstClick)
+	this.GetPitchYawRollDistance = function()
+	{
+		//	dir from lookat to position (orbit, not first person)
+		let Dir = Math.Subtract3( this.Position, this.LookAt );
+		let Distance = Math.Length3( Dir );
+		//Pop.Debug("Distance = ",Distance,Dir);
+		Dir = Math.Normalise3( Dir );
+		
+		let Yaw = Math.RadToDeg( Math.atan2( Dir[0], Dir[2] ) );
+		let Pitch = Math.RadToDeg( Math.asin(-Dir[1]) );
+		let Roll = 0;
+		
+		return [Pitch,Yaw,Roll,Distance];
+	}
+	
+	this.SetOrbit = function(Pitch,Yaw,Roll,Distance)
+	{
+		Pop.Debug("SetOrbit",...arguments);
+		let Pitchr = -Math.radians(Pitch);
+		let Yawr = -Math.radians(Yaw);
+		
+		let Deltax = -Math.sin(Yawr) * Math.cos(Pitchr);
+		let Deltay = Math.sin(Pitchr);
+		let Deltaz = Math.cos(Yawr) * Math.cos(Pitchr);
+		Deltax *= Distance;
+		Deltay *= Distance;
+		Deltaz *= Distance;
+		Pop.Debug( "SetOrbit deltas", Deltax, Deltay, Deltaz );
+		this.Position[0] = this.LookAt[0] + Deltax;
+		this.Position[1] = this.LookAt[1] + Deltay;
+		this.Position[2] = this.LookAt[2] + Deltaz;
+	}
+	
+	this.OnCameraOrbit = function(x,y,z,FirstClick)
+	{
+		let yxz = [y,-x,z];
+		x = yxz[0];
+		y = yxz[1];
+		z = yxz[2];
+		if ( FirstClick )
+		{
+			this.Start_OrbitPyrd = this.GetPitchYawRollDistance();
+			Pop.Debug("this.Start_OrbitPyrd",this.Start_OrbitPyrd);
+			this.Last_OrbitPos = [x,y,z];
+		}
+		
+		let Deltax = this.Last_OrbitPos[0] - x;
+		let Deltay = this.Last_OrbitPos[1] - y;
+		let Deltaz = this.Last_OrbitPos[2] - z;
+	
+		Deltax *= 0.1;
+		Deltay *= 0.1;
+		Deltaz *= 0.1;
+
+		let NewPitch = this.Start_OrbitPyrd[0] + Deltax;
+		let NewYaw = this.Start_OrbitPyrd[1] + Deltay;
+		let NewRoll = this.Start_OrbitPyrd[2] + Deltaz;
+		let NewDistance = this.Start_OrbitPyrd[3];
+		
+		this.SetOrbit( NewPitch, NewYaw, NewRoll, NewDistance );
+	}
+	
 	this.OnCameraPan = function(x,y,z,FirstClick)
 	{
 		if ( FirstClick )
