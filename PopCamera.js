75cf4a82ac12dcc8a902118e7867ef6e0a5806e4
Graham Reeves <graham@grahamreeves.com>
2019-07-25 14:34:39 +0100

    Cleaned up lookmatrix to a rotation & translation that works with our math funcs

--- PopCamera.js
+++ PopCamera.js
@@ -81,123 +81,84 @@ Pop.Camera = function()
 		return Matrix;
 	}
 	
-	function GetLookAtMatrix(eye,up,center)
+	
+	function GetRotationMatrix(eye,up,center)
 	{
-		var eyex = eye[0];
-		var eyey = eye[1];
-		var eyez = eye[2];
-		var upx = up[0];
-		var upy = up[1];
-		var upz = up[2];
-		var centerx = center[0];
-		var centery = center[1];
-		var centerz = center[2];
-	 
-		//if (Math.abs(eyex - centerx) < glMatrix.EPSILON && Math.abs(eyey - centery) < glMatrix.EPSILON && Math.abs(eyez - centerz) < glMatrix.EPSILON) {
-		//	return mat4.identity(out);
-		//}
-	 
-		let z0 = eyex - centerx;
-		let z1 = eyey - centery;
-		let z2 = eyez - centerz;
+		let z = Math.Subtract3( eye, center );
+		z = Math.Normalise3( z );
 		
-		let len = 1 / Math.sqrt(z0 * z0 + z1 * z1 + z2 * z2);
-		z0 *= len;
-		z1 *= len;
-		z2 *= len;
+		let x = Math.Cross3( up, z );
+		x = Math.Normalise3( x );
 		
-		let x0 = upy * z2 - upz * z1;
-		let x1 = upz * z0 - upx * z2;
-		let x2 = upx * z1 - upy * z0;
-		len = Math.sqrt(x0 * x0 + x1 * x1 + x2 * x2);
-		if (!len) {
-			x0 = 0;
-			x1 = 0;
-			x2 = 0;
-		} else {
-			len = 1 / len;
-			x0 *= len;
-			x1 *= len;
-			x2 *= len;
-		}
+		let y = Math.Cross3( z,x );
+		y = Math.Normalise3( y );
 		
-		let y0 = z1 * x2 - z2 * x1;
-		let y1 = z2 * x0 - z0 * x2;
-		let y2 = z0 * x1 - z1 * x0;
+		let tx = 0;
+		let ty = 0;
+		let tz = 0;
 		
-		len = Math.sqrt(y0 * y0 + y1 * y1 + y2 * y2);
-		if (!len) {
-			y0 = 0;
-			y1 = 0;
-			y2 = 0;
-		} else {
-			len = 1 / len;
-			y0 *= len;
-			y1 *= len;
-			y2 *= len;
-		}
-		
-		let out = [];
-		out[0] = x0;
-		out[1] = y0;
-		out[2] = z0;
-		out[3] = 0;
-		out[4] = x1;
-		out[5] = y1;
-		out[6] = z1;
-		out[7] = 0;
-		out[8] = x2;
-		out[9] = y2;
-		out[10] = z2;
-		out[11] = 0;
-		out[12] = -(x0 * eyex + x1 * eyey + x2 * eyez);
-		out[13] = -(y0 * eyex + y1 * eyey + y2 * eyez);
-		out[14] = -(z0 * eyex + z1 * eyey + z2 * eyez);
-		out[15] = 1;
+		let out =
+		[
+		 x[0],	y[0],	z[0],	0,
+		 x[1],	y[1],	z[1],	0,
+		 x[2],	y[2],	z[2],	0,
+		 tx,	ty,	tz,	1,
+		 ];
 		
 		return out;
 	}
+	
+	function GetTranslationMatrix(x,y,z)
+	{
+		return [ 1,0,0,0,	0,1,0,0,	0,0,1,0,	x,y,z,1	];
+	}
+	
+	//	this generates a pos & rot matrix already multiplied together
+	//	would be nice to seperate to be more readable
+	function GetLookAtMatrix(eye,up,center)
+	{
+		let z = Math.Subtract3( eye, center );
+		z = Math.Normalise3( z );
+		
+		let x = Math.Cross3( up, z );
+		x = Math.Normalise3( x );
+		
+		let y = Math.Cross3( z,x );
+		y = Math.Normalise3( y );
+		
+		//	this is the result when multiplying rot*trans matrix
+		//	(dot prod)
+		let tx = -(x[0] * eye[0] + x[1] * eye[1] + x[2] * eye[2]);
+		let ty = -(y[0] * eye[0] + y[1] * eye[1] + y[2] * eye[2]);
+		let tz = -(z[0] * eye[0] + z[1] * eye[1] + z[2] * eye[2]);
+		
+		let out =
+		[
+		 x[0],	y[0],	z[0],	0,
+		 x[1],	y[1],	z[1],	0,
+		 x[2],	y[2],	z[2],	0,
+		 tx,	ty,	tz,	1,
+		];
+		
+		return out;
+	}
+	
 	
 	//	camera's modelview transform
 	this.GetWorldToCameraMatrix = function()
 	{
 		//	https://stackoverflow.com/questions/349050/calculating-a-lookat-matrix
 		let Up = [0,1,0];
-		let Forward = Math.Subtract3( this.LookAt, this.Position );
-		let zaxis = Math.Normalise3( Forward );
-		let Right = Math.Cross3( Up, zaxis );
-		let xaxis = Math.Normalise3( Right );
-		let yaxis = Math.Cross3( zaxis, xaxis );
-		yaxis = Math.Normalise3( yaxis );
 		
-		let tx = this.Position[0];
-		let ty = this.Position[1];
-		let tz = this.Position[2];
+		//	gr: these now match
+		//	but is that translation correct...
+		let Rotation = GetRotationMatrix( this.Position, Up, this.LookAt );
+		let Trans = Math.Subtract3( this.LookAt, this.Position );
+		let Translation = GetTranslationMatrix( Trans[0], Trans[1], Trans[2] );
+		let Matrix = Math.MatrixMultiply4x4( Rotation, Translation );
+		return Matrix;
 		
 		return GetLookAtMatrix( this.Position, Up, this.LookAt );
-		
-		function GetTranslationMatrix(x,y,z)
-		{
-			return [ 1,0,0,0,	0,1,0,0,	0,0,1,0,	x,y,z,1	];
-		}
-		function GetIdentityMatrix(x,y,z)
-		{
-			return GetTranslationMatrix(0,0,0);
-		}
-		
-		let Translation = GetTranslationMatrix( tx, ty, tz );
-		
-		let Rotation =
-		[
-			xaxis[0], yaxis[0], zaxis[0], 0,
-			xaxis[1], yaxis[1], zaxis[1], 0,
-			xaxis[2], yaxis[2], zaxis[2], 0,
-			0, 0, 0, 1
-		];
-		
-		let Matrix = Math.MatrixMultiply4x4( Rotation, Translation );
-		
-		return Matrix;
 	}
 	
 	this.GetLocalToWorldMatrix = function()
