fd578db39c73c374cd8b6293c1cecaa49a47ec24
Graham Reeves <graham@grahamreeves.com>
2019-07-25 14:14:57 +0100

    Finally got a correct lookat() from glmatrix

--- PopCamera.js
+++ PopCamera.js
@@ -3,7 +3,7 @@ Pop.Camera = function()
 {
 	this.FovVertical = 45;
 	
-	this.Position = [ 0,1,20 ];
+	this.Position = [ 0,2,20 ];
 	this.LookAt = [ 0,0,0 ];
 	
 	this.NearDistance = 0.01;
@@ -81,27 +81,135 @@ Pop.Camera = function()
 		return Matrix;
 	}
 	
+	function GetLookAtMatrix(eye,up,center)
+	{
+		var eyex = eye[0];
+		var eyey = eye[1];
+		var eyez = eye[2];
+		var upx = up[0];
+		var upy = up[1];
+		var upz = up[2];
+		var centerx = center[0];
+		var centery = center[1];
+		var centerz = center[2];
+	 
+		//if (Math.abs(eyex - centerx) < glMatrix.EPSILON && Math.abs(eyey - centery) < glMatrix.EPSILON && Math.abs(eyez - centerz) < glMatrix.EPSILON) {
+		//	return mat4.identity(out);
+		//}
+	 
+		let z0 = eyex - centerx;
+		let z1 = eyey - centery;
+		let z2 = eyez - centerz;
+		
+		let len = 1 / Math.sqrt(z0 * z0 + z1 * z1 + z2 * z2);
+		z0 *= len;
+		z1 *= len;
+		z2 *= len;
+		
+		let x0 = upy * z2 - upz * z1;
+		let x1 = upz * z0 - upx * z2;
+		let x2 = upx * z1 - upy * z0;
+		len = Math.sqrt(x0 * x0 + x1 * x1 + x2 * x2);
+		if (!len) {
+			x0 = 0;
+			x1 = 0;
+			x2 = 0;
+		} else {
+			len = 1 / len;
+			x0 *= len;
+			x1 *= len;
+			x2 *= len;
+		}
+		
+		let y0 = z1 * x2 - z2 * x1;
+		let y1 = z2 * x0 - z0 * x2;
+		let y2 = z0 * x1 - z1 * x0;
+		
+		len = Math.sqrt(y0 * y0 + y1 * y1 + y2 * y2);
+		if (!len) {
+			y0 = 0;
+			y1 = 0;
+			y2 = 0;
+		} else {
+			len = 1 / len;
+			y0 *= len;
+			y1 *= len;
+			y2 *= len;
+		}
+		
+		let out = [];
+		out[0] = x0;
+		out[1] = y0;
+		out[2] = z0;
+		out[3] = 0;
+		out[4] = x1;
+		out[5] = y1;
+		out[6] = z1;
+		out[7] = 0;
+		out[8] = x2;
+		out[9] = y2;
+		out[10] = z2;
+		out[11] = 0;
+		out[12] = -(x0 * eyex + x1 * eyey + x2 * eyez);
+		out[13] = -(y0 * eyex + y1 * eyey + y2 * eyez);
+		out[14] = -(z0 * eyex + z1 * eyey + z2 * eyez);
+		out[15] = 1;
+		
+		return out;
+	}
+	
+	//	camera's modelview transform
 	this.GetWorldToCameraMatrix = function()
 	{
 		//	https://stackoverflow.com/questions/349050/calculating-a-lookat-matrix
 		let Up = [0,1,0];
 		let Forward = Math.Subtract3( this.LookAt, this.Position );
 		let zaxis = Math.Normalise3( Forward );
-		let Right = Math.Cross3( Up, zaxis);
+		let Right = Math.Cross3( Up, zaxis );
 		let xaxis = Math.Normalise3( Right );
 		let yaxis = Math.Cross3( zaxis, xaxis );
-		let MinusPos = Math.Subtract3( [0,0,0], this.Position );
-		let tx = -Math.Dot3( xaxis, MinusPos );
-		let ty = -Math.Dot3( yaxis, MinusPos );
-		let tz = -Math.Dot3( zaxis, MinusPos );
-
-		let Matrix =
+		yaxis = Math.Normalise3( yaxis );
+		
+		let tx = this.Position[0];
+		let ty = this.Position[1];
+		let tz = this.Position[2];
+		
+		return GetLookAtMatrix( this.Position, Up, this.LookAt );
+		
+		function GetTranslationMatrix(x,y,z)
+		{
+			return [ 1,0,0,0,	0,1,0,0,	0,0,1,0,	x,y,z,1	];
+		}
+		function GetIdentityMatrix(x,y,z)
+		{
+			return GetTranslationMatrix(0,0,0);
+		}
+		
+		let Translation = GetTranslationMatrix( tx, ty, tz );
+		
+		let Rotation =
 		[
-		 xaxis[0], yaxis[0], zaxis[0], 0,
-		 xaxis[1], yaxis[1], zaxis[1], 0,
-		 xaxis[2], yaxis[2], zaxis[2], 0,
-		 tx, ty, tz, 1
+			xaxis[0], yaxis[0], zaxis[0], 0,
+			xaxis[1], yaxis[1], zaxis[1], 0,
+			xaxis[2], yaxis[2], zaxis[2], 0,
+			0, 0, 0, 1
 		];
+		
+		let Matrix = Math.MatrixMultiply4x4( Rotation, Translation );
+		
+		return Matrix;
+	}
+	
+	this.GetLocalToWorldMatrix = function()
+	{
+		let WorldToCameraMatrix = this.GetWorldToCameraMatrix();
+		
+		//	gr; this SHOULD be inverse...
+		let Matrix = Math.MatrixInverse4x4( LocalToWorld );
+		//let Matrix = LocalToWorld;
+		Pop.Debug("Matrix",Matrix);
+		
+		
 		return Matrix;
 	}
 	
@@ -163,7 +271,7 @@ Pop.Camera = function()
 		Deltax *= 0.1;
 		Deltay *= 0.1;
 		Deltaz *= 0.1;
-
+	
 		let NewPitch = this.Start_OrbitPyrd[0] + Deltax;
 		let NewYaw = this.Start_OrbitPyrd[1] + Deltay;
 		let NewRoll = this.Start_OrbitPyrd[2] + Deltaz;
@@ -187,23 +295,6 @@ Pop.Camera = function()
 		this.LastPos_PanPos = [x,y,z];
 	}
 	
-	this.OnLookAtPan = function(x,y,z,FirstClick)
-	{
-		if ( FirstClick )
-			this.LastPos_PanPos = [x,y,z];
-		
-		let Deltax = this.LastPos_PanPos[0] - x;
-		let Deltay = this.LastPos_PanPos[1] - y;
-		let Deltaz = this.LastPos_PanPos[2] - z;
-		this.Position[0] += Deltax * 0.01
-		this.Position[1] -= Deltay * 0.01
-		this.Position[2] += Deltaz * 0.01
-		
-		this.LastPos_PanPos = [x,y,z];
-	}
-	
-	
-	
 	this.OnCameraZoom = function(x,y,FirstClick)
 	{
 		Pop.Debug("OnCameraZoom deprecated, pass z to CameraPan");
@@ -219,5 +310,7 @@ Pop.Camera = function()
 		this.LastPosZoomPos = [x,y];
 	}
 	
+	
+	Pop.Debug("initial pitch/yaw/roll/distance",this.GetPitchYawRollDistance());
 }
 
