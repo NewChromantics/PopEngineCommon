4e3a6b21158b99148c77910b6e70c0aa6c66b42c
Graham Reeves <graham@grahamreeves.com>
2019-08-19 16:58:20 +0100

    integrating calibrated camera focal lengths with opengl projection matrix

--- PopCamera.js
+++ PopCamera.js
@@ -20,6 +20,139 @@ Pop.Camera = function(CopyCamera)
 		this.Rotation4x4 = CopyCamera.Rotation4x4;
 	}
 
+	
+	
+	this.FieldOfViewToFocalLengths = function(FovHorz,FovVert)
+	{
+		let fx = 363.30 * 2;
+		let s = 0;
+		let fy = 364.19 * 2;
+		let cx = 400;
+		let cy = 400;
+		
+		//	lengths should be in pixels?
+		let Width = 1;
+		let Height = 1;
+		let FocalLengthHorz = Width / Math.tan( Math.radians(FovHorz) / 2);
+		let FocalLengthVert = Height / Math.tan( Math.radians(FovVert) / 2);
+		Pop.Debug('FocalLengthVert',FocalLengthVert,'FocalLengthHorz',FocalLengthHorz);
+		//^^^ FocalLengthHorz=1.816739344621
+		
+		//	needs to convert to -1...1?
+		//	or 0..1
+		//	https://strawlab.org/2011/11/05/augmented-reality-with-OpenGL
+		let ImW = 800;
+		let ImH = 800;
+		let x0 = 0;	//	image center in...opengl?
+		let y0 = 0;	//	image center in...opengl?
+		FocalLengthHorz = 2 * fx / ImW;	//	2*K00/width
+		let GL_s = -2 * s /ImW;			//	-2*K01/width
+		let LensCenterX = (ImW - 2 * cx + 2 * x0)/ImW;	//	(width - 2*K02 + 2*x0)/width
+		//	0
+		let Flip = 1;	//	-1 to flip
+		FocalLengthVert = (Flip*2) * fy / ImH;	//	-2*K11/height
+		let LensCenterY = ((ImH*Flip) - 2 * (Flip*cy) + 2*y0)/ImH;	//	(height - 2*K12 + 2*y0)/height
+		Pop.Debug('FocalLengthVert',FocalLengthVert,'FocalLengthHorz',FocalLengthHorz);
+		
+		let Focal = {};
+		Focal.fx = FocalLengthHorz;
+		Focal.fy = FocalLengthVert;
+		Focal.cx = LensCenterX;
+		Focal.cy = LensCenterY;
+		return Focal;
+	}
+	
+	this.FocalLengthsToFieldOfView = function(fx,fy,cx,cy,ImageWidth,ImageHeight)
+	{
+		let FovHorizontal = Math.RadToDeg( 2 * Math.atan( ImageWidth / (2*fx) ) );
+		let FovVertical = Math.RadToDeg( 2 * Math.atan( ImageHeight / (2*fy) ) );
+		let Fov = {};
+		Fov.Horz = FovHorizontal;
+		Fov.Vert = FovVertical;
+		return Fov;
+	}
+	
+	this.GetFieldOfView = function(ViewRect)
+	{
+		let Fov = {};
+		let Aspect = ViewRect[2] / ViewRect[3];
+		Fov.Horz = this.FovVertical / Aspect;
+		Fov.Vert = this.FovVertical;
+		return Fov;
+	}
+	
+	this.GetPixelFocalLengths = function(ViewRect)
+	{
+		/*
+		let UseFov = true;
+		
+		if ( UseFov )
+		{
+			let Fov = this.GetFieldOfView();
+		}
+		*/
+		let Focal = {};
+		//	from calibration on 800x800 image
+		Focal.fx = 363.30 * 2;
+		Focal.fy = 364.19 * 2;
+		Focal.cx = 400;
+		Focal.cy = 400;
+		Focal.s = 0;
+		
+		return Focal;
+	}
+	
+	this.PixelToOpenglFocalLengths = function(PixelFocals,ImageSize)
+	{
+		//	https://strawlab.org/2011/11/05/augmented-reality-with-OpenGL
+		//	convert pixel focal projection to opengl projection frustum
+		let s = 0;
+		let ImW = ImageSize[0];
+		let ImH = ImageSize[1];
+		
+		
+		let x0 = 0;	//	image center in...opengl?
+		let y0 = 0;	//	image center in...opengl?
+		let FocalLengthHorz = 2 * PixelFocals.fx / ImW;	//	2*K00/width
+		let GL_s = -2 * s /ImW;			//	-2*K01/width
+		let LensCenterX = (ImW - 2 * PixelFocals.cx + 2 * x0)/ImW;	//	(width - 2*K02 + 2*x0)/width
+		//	0
+		let Flip = 1;	//	-1 to flip
+		let FocalLengthVert = (Flip*2) * PixelFocals.fy / ImH;	//	-2*K11/height
+		let LensCenterY = ((ImH*Flip) - 2 * (Flip*PixelFocals.cy) + 2*y0)/ImH;	//	(height - 2*K12 + 2*y0)/height
+		//Pop.Debug('FocalLengthVert',FocalLengthVert,'FocalLengthHorz',FocalLengthHorz);
+		
+		const OpenglFocal = {};
+		OpenglFocal.fx = FocalLengthHorz;
+		OpenglFocal.fy = FocalLengthVert;
+		OpenglFocal.cx = LensCenterX;
+		OpenglFocal.cy = LensCenterY;
+		OpenglFocal.s = GL_s;
+		
+		return OpenglFocal;
+	}
+	
+	this.GetOpenglFocalLengths = function(ViewRect)
+	{
+		/*
+		const Focal = this.GetPixelFocalLengths();
+		//	image size from calibrated focal lengths
+		const ImageWidth = 800;
+		const ImageHeight = 800;
+		const OpenglFocal = this.PixelToOpenglFocalLengths( Focal, [ImageWidth, ImageHeight] );
+		*/
+		const Aspect = ViewRect[2] / ViewRect[3];
+		const FovVertical = this.FovVertical;
+		const FovHorizontal = FovVertical / Aspect;
+		const OpenglFocal = {};
+		OpenglFocal.fx = 1.0 / Math.tan( Math.radians(FovVertical) / 2);
+		OpenglFocal.fy = 1.0 / Math.tan( Math.radians(FovHorizontal) / 2);
+		OpenglFocal.cx = 0;
+		OpenglFocal.cy = 0;
+		OpenglFocal.s = 0;
+		return OpenglFocal;
+	}
+	
 	//	world to pixel
 	this.GetOpencvProjectionMatrix = function(ViewRect)
 	{
@@ -32,30 +165,63 @@ Pop.Camera = function(CopyCamera)
 		 |0  0   1|
 		*/
 		
-		//	from calibration
-		//	on 800x800 image
-		let w = 363.30 * 2;
-		let h = 364.19 * 2;
-		let cx = 400;
-		let cy = 400;
-		
-		/*
-		//	gr: this works far better if square
-		let w = ViewRect[2];
-		//let h = ViewRect[3];
-		let h = w;
-		let cx = w/2;
-		let cy = h/2;
-		*/
+		//	from calibration on 800x800 image
+		const Focal = this.GetPixelFocalLengths();
 		
 		let Matrix =
 		[
-			w, 	0, 	cx,
-		  	0,	h,	cy,
-		  	0, 	0, 	1
+			Focal.fx, 	Focal.s,	Focal.cx,
+		  	0,			Focal.fy,	Focal.cy,
+		  	0, 			0, 			1
 		];
+		/*	test unconvert
+		let w = ViewRect[2];
+		let h = ViewRect[3];
+		
+		let Fov = this.FocalLengthsToFieldOfView( fx, fy, cx, cy, w, h );
+		//let FovHorizontal = Math.RadToDeg( 2 * Math.atan( w / (2*fx) ) );
+		//let FovVertical = Math.RadToDeg( 2 * Math.atan( h / (2*fy) ) );
+		Pop.Debug('opencv camera','FovHorizontal',Fov.Horz,'FovVertical',Fov.Vert);
+		
+		//let FocalLengthHorz = w / Math.tan( Math.radians(FovHorizontal) / 2) / 2;
+		//Pop.Debug("fx",fx,"...",FocalLengthHorz);
+		*/
 		return Matrix;
 	}
+	
+	//	GetOpencvProjectionMatrix but 4x4 with z correction for near/far
+	this.GetProjectionMatrix = function(ViewRect)
+	{
+		const OpenglFocal = this.GetOpenglFocalLengths( ViewRect );
+		
+		const Matrix = [];
+		Matrix[0] = OpenglFocal.fx;
+		Matrix[1] = OpenglFocal.s;
+		Matrix[2] = OpenglFocal.cx;
+		Matrix[3] = 0;
+		
+		Matrix[4] = 0;
+		Matrix[5] = OpenglFocal.fy;
+		Matrix[6] = OpenglFocal.cy;
+		Matrix[7] = 0;
+		
+		const Far = this.FarDistance;
+		const Near = this.NearDistance;
+		
+		//	near...far in opengl needs to resovle to -1...1
+		Matrix[8] = 0;
+		Matrix[9] = 0;
+		Matrix[10] = (Far+Near) / (Near-Far);
+		Matrix[11] = -1;
+		
+		Matrix[12] = 0;
+		Matrix[13] = 0;
+		Matrix[14] = (2*Far*Near) / (Near-Far);
+		Matrix[15] = 0;
+		
+		return Matrix;
+	}
+	
 	
 	this.SetLocalRotationMatrix = function(Rotation4x4)
 	{
@@ -74,45 +240,6 @@ Pop.Camera = function(CopyCamera)
 		const Up = this.GetUp();
 		const Mtx = Math.CreateLookAtRotationMatrix( this.Position, Up, this.LookAt );
 		return Mtx;
-	}
-	
-	this.GetProjectionMatrix = function(ViewRect)
-	{
-		let Aspect = ViewRect[2] / ViewRect[3];
-		
-		//	lengths should be in pixels
-		let FocalLengthVertical = 1.0 / Math.tan( Math.radians(this.FovVertical) / 2);
-		let FocalLengthHorizontal = FocalLengthVertical / Aspect;
-		
-		let LensCenterX = 0;
-		let LensCenterY = 0;
-
-		let Matrix = [];
-		Matrix[0] = FocalLengthHorizontal;
-		Matrix[1] = 0;
-		Matrix[2] = LensCenterX;
-		Matrix[3] = 0;
-		
-		Matrix[4] = 0;
-		Matrix[5] = FocalLengthVertical;
-		Matrix[6] = LensCenterY;
-		Matrix[7] = 0;
-	
-		let Far = this.FarDistance;
-		let Near = this.NearDistance;
-
-		//	near...far in opengl needs to resovle to -1...1
-		Matrix[8] = 0;
-		Matrix[9] = 0;
-		Matrix[10] = (Far+Near) / (Near-Far);
-		Matrix[11] = -1;
-		
-		Matrix[12] = 0;
-		Matrix[13] = 0;
-		Matrix[14] = (2*Far*Near) / (Near-Far);
-		Matrix[15] = 0;
-		
-		return Matrix;
 	}
 	
 	
