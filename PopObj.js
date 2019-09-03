4f1a20b93fd2621d8213c10556bb06a4861ca81d
Graham Reeves <graham@grahamreeves.com>
2019-09-03 16:30:15 +0100

    Added support for an # Scale 0.1 in obj to scale on import

--- PopObj.js
+++ PopObj.js
@@ -10,25 +10,46 @@ Pop.Obj.Parse = function(Contents,OnVertex)
 	Obj.Prefix_Material = 'mtllib ';
 	Obj.Prefix_Object = 'o ';
 	Obj.Prefix_Face = 'f ';
+	Obj.Prefix_Scale = '# Scale ';
 
 	Pop.Debug("Contents.length",Contents.length);
 	const Lines = Contents.split('\n');
 
-	let ParseLine = function(Line)
+	let Scale = 1.0;
+
+	const ParsePositionFloat = function(FloatStr)
 	{
+		let f = parseFloat( FloatStr );
+		f *= Scale;
+		return f;
+	}
+	
+	
+	const ParseLine = function(Line)
+	{
+		Line = Line.trim();
+		
+		//	gr: added a scale key
+		if ( Line.startsWith(Obj.Prefix_Scale) )
+		{
+			Line = Line.replace( Obj.Prefix_Scale,'');
+			Scale = parseFloat( Line );
+			Pop.Debug("Found scale in obj: ",Scale);
+			return;
+		}
+
 		if ( !Line.startsWith(Obj.Prefix_Position) )
 			return;
-		
-		Line = Line.trim();
+
 		let pxyx = Line.split(' ');
 		if ( pxyx.length != 4 )
 		{
-			Pop.Debug("ignoring line " + Line, pxyx.length);
+			Pop.Debug("ignoring line", Line, pxyx.length);
 			return;
 		}
-		let x = parseFloat(pxyx[1]);
-		let y = parseFloat(pxyx[2]);
-		let z = parseFloat(pxyx[3]);
+		let x = ParsePositionFloat( pxyx[1] );
+		let y = ParsePositionFloat( pxyx[2] );
+		let z = ParsePositionFloat( pxyx[3] );
 		OnVertex( x,y,z );
 	}
 	
