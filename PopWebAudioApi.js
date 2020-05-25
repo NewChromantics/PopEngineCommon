c508adb53427f01d65dd252f4e419e0eb6309bb0
Graham Reeves <graham@grahamreeves.com>
2020-05-25 11:04:31 +0100

    Pop.Audio.Sound.Seek() has been removed for Play(Time). Pop.Audio.Sound has been replaced with WebAudio implementation. The old one is now SimpleSound

--- PopWebAudioApi.js
+++ PopWebAudioApi.js
@@ -15,7 +15,8 @@ async function WaitForClick()
 }
 
 
-Pop.Audio.Sound = class
+//	simply play a sound with HTMLAudio objects, no effects
+Pop.Audio.SimpleSound = class
 {
 	constructor(WaveData,Name)
 	{
@@ -53,31 +54,18 @@ Pop.Audio.Sound = class
 			await Action.call(this);
 		}
 	}
-
-	Seek(TimeMs)
-	{
-		const QueueTime = Pop.GetTimeNowMs();
-		function DoSeek()
-		{
-			//this.Sound.pause();	//	not sure if neccessary
-			this.Sound.currentTime = TimeMs / 1000;
-			const Delay = Pop.GetTimeNowMs() - QueueTime;
-			if (Delay > 5)
-				Pop.Debug(`Seek delay ${this.Name} ${Delay.toFixed(2)}ms now: ${this.Sound.currentTime}`);
-		}
-		this.ActionQueue.Push( DoSeek );
-	}
 	
-	Play()
+	Play(TimeMs)
 	{
 		const QueueTime = Pop.GetTimeNowMs();
 		//Pop.Debug(`Queue play(${Name}) at ${Pop.GetTimeNow}
 		async function DoPlay()
 		{
+			this.Sound.currentTime = TimeMs / 1000;
 			await this.Sound.play();
 			const Delay = Pop.GetTimeNowMs() - QueueTime;
 			if ( Delay > 5 )
-				Pop.Debug(`Play delay ${this.Name} ${Delay.toFixed(2)}ms`);
+				Pop.Debug(`Play(${TimeMs.toFixed(2)}) delay ${this.Name} ${Delay.toFixed(2)}ms`);
 		}
 		this.ActionQueue.Push(DoPlay);
 	}
@@ -87,6 +75,110 @@ Pop.Audio.Sound = class
 		async function DoStop()
 		{
 			this.Sound.pause();
+		}
+		this.ActionQueue.Push(DoStop);
+	}
+}
+
+
+//	singleton
+//	gr: could turn this into a single promise that is resolved once and then forever ready
+Pop.Audio.Context = null;
+Pop.Audio.WaitForContext = async function()
+{
+	if (Pop.Audio.Context)
+		return Pop.Audio.Context;
+	
+	//	wait for security
+	await WaitForClick();
+	//	get func
+	const TAudioContext = window.AudioContext || window.webkitAudioContext;
+	Pop.Audio.Context = new TAudioContext();
+	return Pop.Audio.Context;
+}
+
+
+//	more complex WebAudio sound
+Pop.Audio.Sound = class
+{
+	constructor(WaveData,Name)
+	{
+		this.WaveData = WaveData;
+		this.Name = Name;
+		this.ActionQueue = new Pop.PromiseQueue();
+		this.Update().then(Pop.Debug).catch(Pop.Debug);
+		
+		this.SampleBuffer = null;
+		
+		//	webaudio says bufferSource's are one-shot and cheap to make
+		//	and kill themselves off.
+		//	we only need a reference to the last one in case we need to kill it
+		//	or modify the node tree (params on effects)
+		this.CurrentSource = null;
+	}
+	
+	async Update()
+	{
+		//	load
+		const Context = await Pop.Audio.WaitForContext();
+		this.SampleBuffer = await Context.decodeAudioData( this.WaveData.buffer );
+		
+		while (this.SampleBuffer)
+		{
+			const Action = await this.ActionQueue.WaitForNext();
+			await Action.call(this,Context);
+		}
+	}
+	
+	CullCurrentSource()
+	{
+		if ( !this.CurrentSource )
+			return;
+		
+		//	check if it's ended
+		//	warn if still playing, or stop?
+	}
+	
+	CreateSource(Context)
+	{
+		//	create node tree
+		//	todo: keep the tree (reverb, volume etc) in some meta
+		const BufferSource = Context.createBufferSource();
+		BufferSource.buffer = this.SampleBuffer;
+		BufferSource.connect( Context.destination );
+		this.CurrentSource = BufferSource;
+	}
+	
+	Play(TimeMs)
+	{
+		const QueueTime = Pop.GetTimeNowMs();
+		//Pop.Debug(`Queue play(${Name}) at ${Pop.GetTimeNow}
+		async function DoPlay(Context)
+		{
+			this.CullCurrentSource();
+			this.CreateSource(Context);
+
+			//	start!
+			const DelaySecs = 0;
+			const OffsetSecs = TimeMs / 1000;
+			this.CurrentSource.start(DelaySecs,OffsetSecs);
+			
+			//	debug
+			const JobDelay = Pop.GetTimeNowMs() - QueueTime;
+			if ( JobDelay > 5 )
+				Pop.Debug(`Play delay ${this.Name} ${JobDelay.toFixed(2)}ms`);
+		}
+		this.ActionQueue.Push(DoPlay);
+	}
+	
+	Stop()
+	{
+		async function DoStop()
+		{
+			if ( !this.CurrentSource )
+				return;
+			const DelaySecs = 0;
+			this.CurrentSource.stop(DelaySecs);
 		}
 		this.ActionQueue.Push(DoStop);
 	}
