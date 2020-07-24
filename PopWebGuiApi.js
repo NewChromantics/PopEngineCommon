cf4de3055739c7ad11d4c2a4f71faf4eb5533974
Graham Reeves <graham@grahamreeves.com>
2020-07-24 19:28:33 +0100

    Cut the unused code out of GetElementRect

--- PopWebGuiApi.js
+++ PopWebGuiApi.js
@@ -160,43 +160,6 @@ function float2(_x,_y)
 function GetElementRect(Element)
 {
 	return Element.getBoundingClientRect();
-	let absolutePosition = GetElementRect;
-	let el = Element;
-	//	need to cope with scroll, not just getBoundingClientRect :/
-	//	https://stackoverflow.com/a/32623832/355753
-	let
-	found,
-	left = 0,
-	top = 0,
-	width = 0,
-	height = 0,
-	offsetBase = absolutePosition.offsetBase;
-	if (!offsetBase && document.body) {
-		offsetBase = absolutePosition.offsetBase = document.createElement('div');
-		offsetBase.style.cssText = 'position:absolute;left:0;top:0';
-		document.body.appendChild(offsetBase);
-	}
-	if (el && el.ownerDocument === document && 'getBoundingClientRect' in el && offsetBase) {
-		let boundingRect = el.getBoundingClientRect();
-		let baseRect = offsetBase.getBoundingClientRect();
-		found = true;
-		left = boundingRect.left - baseRect.left;
-		top = boundingRect.top - baseRect.top;
-		width = boundingRect.right - boundingRect.left;
-		height = boundingRect.bottom - boundingRect.top;
-	}
-	return {
-	found: found,
-	left: left,
-	top: top,
-	width: width,
-	height: height,
-	right: left + width,
-	bottom: top + height,
-	x: left,
-	y: top,
-	xy: float2(left,top)
-	};
 }
 
 function SetGuiControl_Draggable(Element)
