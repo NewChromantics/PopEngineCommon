944a3df32c5dca1dd0982fec5d6b986d11063089
Graham Reeves <graham@grahamreeves.com>
2020-10-02 18:34:11 +0100

    Webapi: Fixed messsages being OOO, but has introduced a slight lag where we now process each packet on a thread. Also converted to class.

--- PopWebSocketApi.js
+++ PopWebSocketApi.js
@@ -17,27 +17,71 @@ function GetWebsocketError(Event)
 
 
 //	wrapper for websocket
-Pop.Websocket.Client = function(Hostname,Port=80)
+Pop.Websocket.Client = class
 {
-	this.OnConnectPromises = new Pop.PromiseQueue();
-	this.OnMessagePromises = new Pop.PromiseQueue();
+	constructor(Hostname,Port=80)
+	{
+		this.OnConnectPromises = new Pop.PromiseQueue('WebsocketClient Connects');
+		this.OnMessagePromises = new Pop.PromiseQueue('WebsocketClient Messages');
 	
-	this.WaitForConnect = async function()
+		//	because we need messages to stay in order, but blobs(binary) needs to be async processed
+		//	we need to keep the data in order and process the data seperately on a thread
+		this.PendingMessageData = new Pop.PromiseQueue('WebsocketClient PendingMessages');
+		
+		this.ProcessPendingMessageDataThread().catch(this.OnError.bind(this));
+						
+		//	create a socket
+		//	we don't handle reconnecting, assume user is using Pop.Websocket.Connect
+		//	and when connect or message throws, this is discarded and it connects again
+
+		//	parse hostname in case something odd is put in
+		//	native doesnt support this!
+		//	ws://hello:port
+		const Pattern = '^(ws:\/\/|wss:\/\/)?([^:]+)(:([0-9]+))?$';
+		const Parts = Hostname.split(new RegExp(Pattern));
+
+		//	if no match (array of 1==original), just continue as before
+		if (Parts.length > 1)
+		{
+			let [Prefix,Protocol,NewHostname,ColonAndPort,NewPort,Suffic] = Parts;
+			//	this will be undefined if we just have ws://
+			if (NewHostname !== undefined)
+			{
+				const OldHostname = Hostname;
+				//	fill defaults where not present
+				Protocol = Protocol || '';
+				Port = NewPort || Port;
+				Hostname = `${Protocol}${NewHostname}`;
+				Pop.Debug(`Parsed websocket address ${OldHostname} to Hostname=${Hostname} Port=${Port}`);
+			}
+		}
+
+		let ServerAddress = `${Hostname}:${Port}`;
+		if (!ServerAddress.startsWith('ws://') && !ServerAddress.startsWith('wss://'))
+			ServerAddress = 'ws://' + ServerAddress;
+		this.Socket = new WebSocket(ServerAddress);
+		this.Socket.onopen = this.OnConnected.bind(this);
+		this.Socket.onerror = this.OnError.bind(this);
+		this.Socket.onclose = this.OnDisconnected.bind(this);
+		this.Socket.onmessage = this.OnMessage.bind(this);
+	}
+	
+	async WaitForConnect()
 	{
 		return this.OnConnectPromises.WaitForNext();
 	}
 	
-	this.WaitForMessage = async function()
+	async WaitForMessage()
 	{
 		return this.OnMessagePromises.WaitForNext();
 	}
 	
-	this.OnConnected = function(Event)
+	OnConnected(Event)
 	{
 		this.OnConnectPromises.Push(Event);
 	}
 	
-	this.OnError = function(Event)
+	OnError(Event)
 	{
 		const Error = GetWebsocketError(Event);
 		//	gr: on connection error, error gets invoked first, with no useful info
@@ -46,7 +90,7 @@ Pop.Websocket.Client = function(Hostname,Port=80)
 		this.OnMessagePromises.Reject(Error);
 	}
 	
-	this.OnDisconnected = function(Event)
+	OnDisconnected(Event)
 	{
 		//	OnError is just for messages, but in case that doesnt get triggered,
 		//	clear messages too
@@ -55,45 +99,46 @@ Pop.Websocket.Client = function(Hostname,Port=80)
 		this.OnMessagePromises.Reject(Error);
 	}
 	
-	this.OnMessage = function(Event)
+	OnMessage(Event)
 	{
 		const Data = Event.data;
 		const Packet = {};
 		Packet.Peer = this.GetPeers()[0];
-		Packet.Data = null;
+		Packet.Data = Data;
+		Packet.RecieveTime = Pop.GetTimeNowMs();
 
-		//	if we get a blob, convert to array (no blobs in normal API)
-		if ( typeof Data == 'string' )
-		{
-			Packet.Data = Data;
-			this.OnMessagePromises.Push(Packet);
-			return;
-		}
-		
-		if ( Data instanceof Blob )
-		{
-			const ConvertData = async function()
-			{
-				const DataArrayBuffer = await Data.arrayBuffer();
-				const DataArray = new Uint8Array(DataArrayBuffer);
-				Packet.Data = DataArray;
-				this.OnMessagePromises.Push( Packet );
-			}.bind(this);
-			
-			ConvertData().then().catch( this.OnError.bind(this) );
-			return;
-		}
-		
-		throw "Unhandled type of websocket message; " + Data + " (" + (typeof Data) + ")";
+		//	gr: doing immediate string, and async blob->array buffer means
+		//		one is queued immediately, and one is not, so data gets out of order
+		this.PendingMessageData.Push(Packet);
 	}
 	
-	this.GetPeers = function()
+	async ProcessPendingMessageDataThread()
+	{
+		while ( this.PendingMessageData )
+		{
+			const Packet = await this.PendingMessageData.WaitForNext();
+			
+			//	if we get a blob, convert to array (no blobs in normal API)
+			//	but we do it here, to make sure we keep messages in order
+			if ( Packet.Data instanceof Blob )
+			{
+				const DataArrayBuffer = await Packet.Data.arrayBuffer();
+				const DataArray = new Uint8Array(DataArrayBuffer);
+				Packet.Data = DataArray;
+			}
+			//	else if ! string throw "Unhandled type of websocket message; " + Data + " (" + (typeof Data) + ")";
+			
+			this.OnMessagePromises.Push(Packet);
+		}
+	}
+	
+	GetPeers()
 	{
 		//	this should only return once connected
 		return ['Server'];
 	}
 	
-	this.Send = function(Peer,Message)
+	Send(Peer,Message)
 	{
 		if ( !this.Socket )
 			throw "Todo: Socket not created. Create a promise on connection to send this message";
@@ -104,40 +149,6 @@ Pop.Websocket.Client = function(Hostname,Port=80)
 		this.Socket.send( Message );
 	}
 	
-	//	create a socket
-	//	we don't handle reconnecting, assume user is using Pop.Websocket.Connect
-	//	and when connect or message throws, this is discarded and it connects again
-
-	//	parse hostname in case something odd is put in
-	//	native doesnt support this!
-	//	ws://hello:port
-	const Pattern = '^(ws:\/\/|wss:\/\/)?([^:]+)(:([0-9]+))?$';
-	const Parts = Hostname.split(new RegExp(Pattern));
-
-	//	if no match (array of 1==original), just continue as before
-	if (Parts.length > 1)
-	{
-		let [Prefix,Protocol,NewHostname,ColonAndPort,NewPort,Suffic] = Parts;
-		//	this will be undefined if we just have ws://
-		if (NewHostname !== undefined)
-		{
-			const OldHostname = Hostname;
-			//	fill defaults where not present
-			Protocol = Protocol || '';
-			Port = NewPort || Port;
-			Hostname = `${Protocol}${NewHostname}`;
-			Pop.Debug(`Parsed websocket address ${OldHostname} to Hostname=${Hostname} Port=${Port}`);
-		}
-	}
-
-	let ServerAddress = `${Hostname}:${Port}`;
-	if (!ServerAddress.startsWith('ws://') && !ServerAddress.startsWith('wss://'))
-		ServerAddress = 'ws://' + ServerAddress;
-	this.Socket = new WebSocket(ServerAddress);
-	this.Socket.onopen = this.OnConnected.bind(this);
-	this.Socket.onerror = this.OnError.bind(this);
-	this.Socket.onclose = this.OnDisconnected.bind(this);
-	this.Socket.onmessage = this.OnMessage.bind(this);
 }
 
 //	asynchronously returns a websocket client once it connects
