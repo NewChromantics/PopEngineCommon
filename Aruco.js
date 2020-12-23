301fed20f3c4133fd6c6aa10900e8058fa83c5c1
Graham Reeves <graham@grahamreeves.com>
2020-12-23 17:28:58 +0000

    Outputting correction rotation

--- Aruco.js
+++ Aruco.js
@@ -13,6 +13,92 @@ let DetectorInstance = null;
 let PositInstance = null;
 let PositInstanceMarkerSize = null;
 
+
+//	https://github.com/immersive-web/webxr-polyfill/blob/0202e9d2b80fcce3d46010f21869b8684da9c4f5/build/webxr-polyfill.js#L340
+function getRotation(out, mat) {
+  let trace = mat[0] + mat[5] + mat[10];
+  let S = 0;
+  if (trace > 0) {
+    S = Math.sqrt(trace + 1.0) * 2;
+    out[3] = 0.25 * S;
+    out[0] = (mat[6] - mat[9]) / S;
+    out[1] = (mat[8] - mat[2]) / S;
+    out[2] = (mat[1] - mat[4]) / S;
+  } else if ((mat[0] > mat[5]) && (mat[0] > mat[10])) {
+    S = Math.sqrt(1.0 + mat[0] - mat[5] - mat[10]) * 2;
+    out[3] = (mat[6] - mat[9]) / S;
+    out[0] = 0.25 * S;
+    out[1] = (mat[1] + mat[4]) / S;
+    out[2] = (mat[8] + mat[2]) / S;
+  } else if (mat[5] > mat[10]) {
+    S = Math.sqrt(1.0 + mat[5] - mat[0] - mat[10]) * 2;
+    out[3] = (mat[8] - mat[2]) / S;
+    out[0] = (mat[1] + mat[4]) / S;
+    out[1] = 0.25 * S;
+    out[2] = (mat[6] + mat[9]) / S;
+  } else {
+    S = Math.sqrt(1.0 + mat[10] - mat[0] - mat[5]) * 2;
+    out[3] = (mat[1] - mat[4]) / S;
+    out[0] = (mat[8] + mat[2]) / S;
+    out[1] = (mat[6] + mat[9]) / S;
+    out[2] = 0.25 * S;
+  }
+  return out;
+}
+
+function mat4_invert(out, a) {
+    var a00 = a[0],
+        a01 = a[1],
+        a02 = a[2],
+        a03 = a[3],
+        a10 = a[4],
+        a11 = a[5],
+        a12 = a[6],
+        a13 = a[7],
+        a20 = a[8],
+        a21 = a[9],
+        a22 = a[10],
+        a23 = a[11],
+        a30 = a[12],
+        a31 = a[13],
+        a32 = a[14],
+        a33 = a[15],
+        b00 = a00 * a11 - a01 * a10,
+        b01 = a00 * a12 - a02 * a10,
+        b02 = a00 * a13 - a03 * a10,
+        b03 = a01 * a12 - a02 * a11,
+        b04 = a01 * a13 - a03 * a11,
+        b05 = a02 * a13 - a03 * a12,
+        b06 = a20 * a31 - a21 * a30,
+        b07 = a20 * a32 - a22 * a30,
+        b08 = a20 * a33 - a23 * a30,
+        b09 = a21 * a32 - a22 * a31,
+        b10 = a21 * a33 - a23 * a31,
+        b11 = a22 * a33 - a23 * a32,
+    det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+    if (!det) {
+      return null;
+    }
+    det = 1.0 / det;
+    out[0] = (a11 * b11 - a12 * b10 + a13 * b09) * det;
+    out[1] = (a02 * b10 - a01 * b11 - a03 * b09) * det;
+    out[2] = (a31 * b05 - a32 * b04 + a33 * b03) * det;
+    out[3] = (a22 * b04 - a21 * b05 - a23 * b03) * det;
+    out[4] = (a12 * b08 - a10 * b11 - a13 * b07) * det;
+    out[5] = (a00 * b11 - a02 * b08 + a03 * b07) * det;
+    out[6] = (a32 * b02 - a30 * b05 - a33 * b01) * det;
+    out[7] = (a20 * b05 - a22 * b02 + a23 * b01) * det;
+    out[8] = (a10 * b10 - a11 * b08 + a13 * b06) * det;
+    out[9] = (a01 * b08 - a00 * b10 - a03 * b06) * det;
+    out[10] = (a30 * b04 - a31 * b02 + a33 * b00) * det;
+    out[11] = (a21 * b02 - a20 * b04 - a23 * b00) * det;
+    out[12] = (a11 * b07 - a10 * b09 - a12 * b06) * det;
+    out[13] = (a00 * b09 - a01 * b07 + a02 * b06) * det;
+    out[14] = (a31 * b01 - a30 * b03 - a32 * b00) * det;
+    out[15] = (a20 * b03 - a21 * b01 + a22 * b00) * det;
+    return out;
+  }
+
 //	todo: make this detection async so we can match native API
 //		and spin off onto a worker thread
 async function DetectMarkers(imageData,MarkerSizeMetres=0.10)
@@ -75,13 +161,36 @@ async function DetectMarkers(imageData,MarkerSizeMetres=0.10)
 		const Rotation3x3 = Pose.bestRotation;
 		const Translation3 = Pose.bestTranslation.map(MetresToMM);
 		
+		const Rotation4x4 = 
+		[
+			Rotation3x3[0][0],	Rotation3x3[0][1],	Rotation3x3[0][2],	0,
+			Rotation3x3[1][0],	Rotation3x3[1][1],	Rotation3x3[1][2],	0,
+			Rotation3x3[2][0],	Rotation3x3[2][1],	Rotation3x3[2][2],	0,
+			0,					0,					0,					1
+		];	
+		const Rotation4x4_Transposed = 
+		[
+			Rotation3x3[0][0],	Rotation3x3[1][0],	Rotation3x3[2][0],	0,
+			Rotation3x3[0][1],	Rotation3x3[1][1],	Rotation3x3[2][1],	0,
+			Rotation3x3[0][2],	Rotation3x3[1][2],	Rotation3x3[2][2],	0,
+			0,					0,					0,					1
+		];		
+		
+		//getRotation(Quaternion,Rotation4x4);
+		//getRotation(Quaternion,Rotation4x4_Transposed);
+		let Rotation = Rotation4x4_Transposed;
+		Rotation = mat4_invert( [], Rotation );
+		
+		/*
 		const yaw = -Math.atan2(Rotation3x3[0][2], Rotation3x3[2][2]);
 		const pitch = -Math.asin(-Rotation3x3[1][2]);
 		const roll = Math.atan2(Rotation3x3[1][0], Rotation3x3[1][1]);
 		
 		Pop.Debug(`Rotation3x3=${Rotation3x3} Translation3=${Translation3} pitch=${pitch} yaw=${yaw} roll=${roll}`);
-		
-		Pose.Rotation = [0,0,0,1];
+		*/
+		const Quaternion = [0,0,0,1];
+		getRotation(Quaternion,Rotation);
+		Pose.Rotation = Quaternion;
 		
 		//	gr: these I think are in mm
 		//	todo: change all units to m. Shouldn't matter to the pose code
