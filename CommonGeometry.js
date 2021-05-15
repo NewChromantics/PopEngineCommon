11fabe62410637731c6888233b557eea2179abf7
Graham Reeves <graham@grahamreeves.com>
2021-05-15 18:47:11 +0100

    Adding a module for useful dumb-geometry-creation functions (create quad, cube etc)

---
+++ CommonGeometry.js
@@ -0,0 +1,81 @@
+const Default = `Common geometry generating functions`;
+export default Default;
+
+
+export function CreateCubeGeometry(Min=-1,Max=1)
+{
+	let VertexSize = 3;
+	let VertexData = [];
+	
+	let AddTriangle = function(a,b,c)
+	{
+		let FirstTriangleIndex = VertexData.length / VertexSize;
+		
+		a.forEach( v => VertexData.push(v) );
+		b.forEach( v => VertexData.push(v) );
+		c.forEach( v => VertexData.push(v) );
+	}
+	
+	let tln = [Min,Min,Min];
+	let trn = [Max,Min,Min];
+	let brn = [Max,Max,Min];
+	let bln = [Min,Max,Min];
+	let tlf = [Min,Min,Max];
+	let trf = [Max,Min,Max];
+	let brf = [Max,Max,Max];
+	let blf = [Min,Max,Max];
+	
+	
+	//	near
+	AddTriangle( tln, trn, brn );
+	AddTriangle( brn, bln, tln );
+	//	far
+	AddTriangle( trf, tlf, blf );
+	AddTriangle( blf, brf, trf );
+	
+	//	top
+	AddTriangle( tln, tlf, trf );
+	AddTriangle( trf, trn, tln );
+	//	bottom
+	AddTriangle( bln, blf, brf );
+	AddTriangle( brf, brn, bln );
+	
+	//	left
+	AddTriangle( tlf, tln, bln );
+	AddTriangle( bln, blf, tlf );
+	//	right
+	AddTriangle( trn, trf, brf );
+	AddTriangle( brf, brn, trn );
+	
+	//	loads much faster as a typed array
+	VertexData = new Float32Array( VertexData );
+	
+	const Attributes = {};
+	Attributes.LocalPosition = {};
+	Attributes.LocalPosition.Size = VertexSize;
+	Attributes.LocalPosition.Data = VertexData;
+
+	Attributes.LocalUv = {};
+	Attributes.LocalUv.Size = VertexSize;
+	Attributes.LocalUv.Data = VertexData;
+	
+	return Attributes;
+}
+
+
+export function CreateBlitQuadGeometry()
+{
+	let l = 0;
+	let t = 0;
+	let r = 1;
+	let b = 1;
+	const VertexData = [	l,t,	r,t,	r,b,	r,b, l,b, l,t	];
+	
+	const TexCoord = {};
+	TexCoord.Size = 2;
+	TexCoord.Data = VertexData;
+
+	const Geometry = {};
+	Geometry.TexCoord = TexCoord;
+	return Geometry;
+}
