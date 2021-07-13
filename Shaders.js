6ed3052a1d2463d90168143d9e4a6b36ea1b79b1
Graham Reeves <graham@grahamreeves.com>
2021-07-13 20:35:37 +0100

    Temp fix for a bug in native renderer; dont enumerate two uniforms with the same name from a shader

--- Shaders.js
+++ Shaders.js
@@ -8,6 +8,21 @@ export function ExtractShaderUniforms(Shader,Shader2=null)
 	const Pattern = new RegExp(`uniform\\s([a-zA-Z0-9]+)\\s([a-zA-Z0-9]+)\\s?;`,'g');
 	const Uniforms = [];
 	
+	//	filter out duplicates
+	//	gr: on native was causing some not to be written;
+	//		I think uniform buffer has a bug (webgl okay as theyre set directly)
+	function PushUniqueUniform(Uniform)
+	{
+		function MatchName(ExistingUniform)
+		{
+			return Uniform.Name == ExistingUniform.Name;
+		}
+		//	skip duplicate
+		if ( Uniforms.some(MatchName) )
+			return;
+		Uniforms.push( Uniform );
+	}
+	
 	for ( let i=0;	i<999;	i++ )	//	avoid infinite loop
 	{
 		const Match = Pattern.exec(Shader);
@@ -17,14 +32,14 @@ export function ExtractShaderUniforms(Shader,Shader2=null)
 		const Uniform = {};
 		Uniform.Type = Match[1];
 		Uniform.Name = Match[2];
-		Uniforms.push(Uniform);
+		PushUniqueUniform(Uniform);
 	}
 	
 	//	recurse for multiple inputs
 	if ( Shader2 )
 	{
 		const Uniforms2 = ExtractShaderUniforms(Shader2);
-		Uniforms.push( ...Uniforms2 );
+		Uniforms2.forEach( PushUniqueUniform );
 	}
 	
 	return Uniforms;
