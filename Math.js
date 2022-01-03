719dff3f56ad26cdd03c54fe3fee458de115263b
Graham Reeves <graham@grahamreeves.com>
2022-01-03 13:05:27 +0000

    Added CalcHomography

--- Math.js
+++ Math.js
@@ -2096,3 +2096,177 @@ void GetLineLineIntersection3Clamped(float3 StartA,float3 EndA,float3 StartB,flo
 	IntersectionTimeB = Clamp01(IntersectionTimeB);
 }
 */
+
+
+
+function GaussianElimination(A,n)
+{
+	// originally by arturo castro - 08/01/2010
+	//
+	// ported to c from pseudocode in
+	// http://en.wikipedia.org/wiki/Gaussian_elimination
+	
+	let i = 0;
+	let m = n - 1;
+	for ( let j=0;	j<n && i<m;	j++ )
+	{
+		// Find pivot in column j, starting in row i:
+		let maxi = i;
+		for (let k = i + 1; k < m; k++)
+		{
+			let a_k_j = Math.abs(A[k][j]);
+			let a_maxi_j = Math.abs(A[maxi][j]);
+			if (a_k_j > a_maxi_j)
+			{
+				maxi = k;
+			}
+		}
+		let a_maxi_j = A[maxi][j];
+		if (a_maxi_j != 0)
+		{
+			//console.log( a_maxi_j + " != 0" );
+			//swap rows i and maxi, but do not change the value of i
+			if (i != maxi)
+			{
+				for (let k = 0; k < n; k++)
+				{
+					let aux = A[i][k];
+					A[i][k] = A[maxi][k];
+					A[maxi][k] = aux;
+				}
+			}
+			//Now A[i,j] will contain the old value of A[maxi,j].
+			//divide each entry in row i by A[i,j]
+			let A_ij = A[i][j];
+			for (let k = 0; k < n; k++)
+			{
+				A[i][k] /= A_ij;
+			}
+			//Now A[i,j] will have the value 1.
+			for (let u = i + 1; u < m; u++)
+			{
+				//subtract A[u,j] * row i from row u
+				let A_uj = A[u][j];
+				for (let k = 0; k < n; k++)
+				{
+					let neg = A_uj * A[i][k];
+					let auk = A[u][k];
+					A[u][k] = auk - neg;
+				}
+				//Now A[u,j] will be 0, since A[u,j] - A[i,j] * A[u,j] = A[u,j] - 1 * A[u,j] = 0.
+			}
+			
+			i++;
+		}
+	}
+	
+	//back substitution
+	for (let k = m - 2; k >= 0; k--)
+	{
+		for (let l = k + 1; l < n - 1; l++)
+		{
+			A[k][m] -= A[k][l] * A[l][m];
+			//A[i*n+j]=0;
+		}
+	}
+}
+	
+export function CalcHomography(src,dest,Plane='xy')
+{
+	function ArrayToXy(xy)
+	{
+		if ( xy.x !== undefined )
+			return xy;
+		const x = xy[0];
+		const y = xy[1];
+		xy = {};
+		xy.x = x;
+		xy.y = y;
+		return xy;
+	}
+	src = src.map(ArrayToXy);
+	dest = dest.map(ArrayToXy);
+	
+	// originally by arturo castro - 08/01/2010
+	//
+	// create the equation system to be solved
+	//
+	// from: Multiple View Geometry in Computer Vision 2ed
+	//       Hartley R. and Zisserman A.
+	//
+	// x' = xH
+	// where H is the homography: a 3 by 3 matrix
+	// that transformed to inhomogeneous coordinates for each point
+	// gives the following equations for each point:
+	//
+	// x' * (h31*x + h32*y + h33) = h11*x + h12*y + h13
+	// y' * (h31*x + h32*y + h33) = h21*x + h22*y + h23
+	//
+	// as the homography is scale independent we can let h33 be 1 (indeed any of the terms)
+	// so for 4 points we have 8 equations for 8 terms to solve: h11 - h32
+	// after ordering the terms it gives the following matrix
+	// that can be solved with gaussian elimination:
+	
+	
+	let P = [
+			 [-src[0].x, -src[0].y, -1,   0,   0,  0, src[0].x*dest[0].x, src[0].y*dest[0].x, -dest[0].x ], // h11
+			 [  0,   0,  0, -src[0].x, -src[0].y, -1, src[0].x*dest[0].y, src[0].y*dest[0].y, -dest[0].y ], // h12
+			 
+			 [-src[1].x, -src[1].y, -1,   0,   0,  0, src[1].x*dest[1].x, src[1].y*dest[1].x, -dest[1].x ], // h13
+			 [  0,   0,  0, -src[1].x, -src[1].y, -1, src[1].x*dest[1].y, src[1].y*dest[1].y, -dest[1].y ], // h21
+			 
+			 [-src[2].x, -src[2].y, -1,   0,   0,  0, src[2].x*dest[2].x, src[2].y*dest[2].x, -dest[2].x ], // h22
+			 [  0,   0,  0, -src[2].x, -src[2].y, -1, src[2].x*dest[2].y, src[2].y*dest[2].y, -dest[2].y ], // h23
+			 
+			 [-src[3].x, -src[3].y, -1,   0,   0,  0, src[3].x*dest[3].x, src[3].y*dest[3].x, -dest[3].x ], // h31
+			 [  0,   0,  0, -src[3].x, -src[3].y, -1, src[3].x*dest[3].y, src[3].y*dest[3].y, -dest[3].y ], // h32
+			 ];
+	
+	GaussianElimination( P, 9);
+	
+
+	let Row0,Row1,Row2,Row3;
+	
+	//	gr: to let us invert, need determinet to be non zero
+	let m22 = 1;
+	//	gr: don't know why this is transposed (in C and JS) compared to c#! maybe accessors are different for double arrays? madness
+	let Transpose = true;
+	
+	if (Transpose && Plane == 'xy' )
+	{
+		//	z = identity
+		Row0 = [ P[0][8], P[1][8],	0, P[2][8] ];
+		Row1 = [ P[3][8], P[4][8],	0, P[5][8] ];
+		Row2 = [ 0, 		0, 		m22, 0 ];
+		Row3 = [ P[6][8], P[7][8],	0, 1 ];
+	}
+	else if (Plane == 'xy' )
+	{
+		//	z = identity
+		Row0 = [ P[0][8], P[3][8],	0, P[6][8] ];
+		Row1 = [ P[1][8], P[4][8],	0, P[7][8] ];
+		Row2 = [ 0, 		0, 		m22, 0 ];
+		Row3 = [ P[2][8], P[5][8],	0, 1 ];
+	}
+	else if ( Plane == 'xz' )
+	{
+		//	y = identity
+		Row0 = [ P[0][8],	0,		P[3][8],	P[6][8] ];
+		Row1 = [ 0,			m22,	0,			0		 ];
+		Row2 = [ P[1][8],	0, 		P[4][8],	P[7][8] ];
+		Row3 = [ P[2][8],	0,		P[5][8],	1 ];
+	}
+	
+	//	if we setrow() for each, we'll get an exception as unity checks validity of the matrix
+	//let HomographyMtx = new Matrix4x4(Rows4[0], Rows4[1], Rows4[2], Rows4[3]);
+	const HomographyMtx = 
+	[
+		...Row0,
+		...Row1,
+		...Row2,
+		...Row3,
+	];
+	
+	return HomographyMtx;
+}
+
