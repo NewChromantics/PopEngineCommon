2debd1f5b359455b8d0d4954c3ba00d69004e03a
Graham Reeves <graham@grahamreeves.com>
2022-03-14 16:14:18 +0000

    Added LoadFileAsJsonAsync

--- FileSystem.js
+++ FileSystem.js
@@ -325,6 +325,12 @@ export async function LoadFileAsStringAsync(Filename)
 	return Contents;
 }
 
+export async function LoadFileAsJsonAsync(Filename)
+{
+	const String = await LoadFileAsStringAsync(Filename);
+	const Json = JSON.parse(String);
+	return Json;
+}
 
 export async function LoadFileAsArrayBufferAsync(Filename)
 {
