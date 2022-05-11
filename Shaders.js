4b345afc80faa154b550dfceb14c1d0935a9b10c
Graham Reeves <graham@grahamreeves.com>
2022-05-11 16:54:50 +0100

    Extract meta from comments for uniforms & attributes in shaders

--- Shaders.js
+++ Shaders.js
@@ -1,11 +1,54 @@
 const Default = `Common shader utilities`;
 export default Default;
 
+function CommentToJson(String)
+{
+	if ( !String )
+		return;
+
+	//	strip comment strings
+	//	todo: do nice comment parsing (done this in another project!)
+	String = String.replaceAll('/*','');
+	String = String.replaceAll('*/','');
+	String = String.replaceAll('//','');
+	String = String.trim();
+	if ( !String.length )
+		return null;
+	
+	
+	//	insert "key" quotes around keys
+	//		min:10 -> "min":10
+	//		min=10 -> "min":10
+	//		min    = 10 -> "min": 10
+	//	https://stackoverflow.com/a/44562916/355753
+	//String = String.replace(/([a-zA-Z0-9-]+):([a-zA-Z0-9-]+)/g, "\"$1\":\"$2\"");
+	//String = String.replace(/([a-zA-Z0-9-]+):([a-zA-Z0-9-]+)/g, "\"$1\":$2");
+	String = String.replace(/([a-zA-Z0-9-]+)\s*[:|=]/g, "\"$1\":");
+	
+	//	if the string doesn't start with {
+	//	wrap it, so we can allow
+	//		//	min:1,max:10
+	//	to be turned into {min:1,max:10}
+	if ( String[0] != '{' )
+		String = `{${String}}`;
+	
+	try
+	{
+		let Json = JSON.parse( String );
+		return Json;
+	}
+	catch(e)
+	{
+		console.log(`Failed to convert comment "${String}" to json; ${e}`);
+		return;
+	}
+}
 
 //	could work for varying, uniform, attribute... 
 function ExtractShaderSymbols(Prefix,Shader,Shader2=null)
 {
-	const Pattern = new RegExp(`${Prefix}\\s([a-zA-Z0-9]+)\\s([a-zA-Z0-9_]+)\\s?;`,'g');
+	//	end of this pattern should be line feed or end of doc, but unlikely to get a uniform or attribute at the bottom
+	const Pattern = new RegExp(`${Prefix}\\s([a-zA-Z0-9]+)\\s([a-zA-Z0-9_]+)\\s?;(.*)\n`,'g');
 	const Uniforms = [];
 	
 	//	filter out duplicates
@@ -30,8 +73,16 @@ function ExtractShaderSymbols(Prefix,Shader,Shader2=null)
 			break;
 		//Pop.Debug(`Match = ${JSON.stringify(Match)}`);
 		const Uniform = {};
+		
+		Uniform.Comment = Match[3];
+		const CommentJson = CommentToJson(Uniform.Comment) || {};
+		Object.assign( Uniform, CommentJson );
+		
+		//	force these over any user-provided meta
 		Uniform.Type = Match[1];
 		Uniform.Name = Match[2];
+		Uniform.Comment = Match[3];		
+		
 		PushUniqueUniform(Uniform);
 	}
 	
@@ -48,10 +99,10 @@ function ExtractShaderSymbols(Prefix,Shader,Shader2=null)
 
 export function ExtractShaderUniforms(Shader,Shader2=null)
 {
-	return ExtractShaderSymbols('uniform',Shader);
+	return ExtractShaderSymbols('uniform',...arguments);
 }
 
 export function ExtractShaderAttributes(Shader,Shader2=null)
 {
-	return ExtractShaderSymbols('attribute',Shader);
+	return ExtractShaderSymbols('attribute',...arguments);
 }
